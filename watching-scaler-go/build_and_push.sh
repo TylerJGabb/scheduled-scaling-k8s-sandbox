0a63758dade5863c32b591586126a818b183d1ed
Tyler Gabb <tylerjgabb@gmail.com>
2024-12-22 16:00:43 -0700

    push latest

--- watching-scaler-go/build_and_push.sh
+++ watching-scaler-go/build_and_push.sh
@@ -1,6 +1,8 @@
 . ../.env
-TAG=v8
+TAG=v9
 echo $FQIN:$TAG
 docker build --platform=linux/amd64 -t watching-scaler .
 docker tag watching-scaler $FQIN:$TAG
+docker tag watching-scaler $FQIN:latest
 docker push $FQIN:$TAG
+docker push $FQIN:latest
